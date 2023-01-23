aa512051494f4f27c0d111760ddaeaed71624913
ShuntaIto <shunta.ito@microsoft.com>
2023-01-23 16:46:06 +0900

    Add fluent ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { Fabric } from '@fluentui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import 'modern-normalize'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Fabric>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Next.js + Fluent UI Example</title>
+      </Head>
+      <Component {...pageProps} />
+    </Fabric>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable react/no-danger */
+import * as React from 'react'
+import { Stylesheet, InjectionMode } from '@uifabric/merge-styles'
+import { resetIds } from '@uifabric/utilities'
+import Document, { DocumentContext } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+// Do this in file scope to initialize the stylesheet before Fluent UI React components are imported.
+const stylesheet = Stylesheet.getInstance()
+
+// Set the config.
+stylesheet.setConfig({
+  injectionMode: InjectionMode.none,
+  namespace: 'server'
+})
+
+// Now set up the document, and just reset the stylesheet.
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    stylesheet.reset()
+    resetIds()
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [initialProps.styles, <style key="fluentui-css" dangerouslySetInnerHTML={{ __html: stylesheet.getRules(true) }} />]
+    }
+  }
 }
+
+export default MyDocument
