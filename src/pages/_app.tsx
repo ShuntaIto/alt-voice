a3803ac6edefd1f60e75911e9d585f1faed68a41
ShuntaIto <shunta.ito@microsoft.com>
2023-01-24 14:29:29 +0900

    Update to fluet ui v9

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react'
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import { Fabric } from '@fluentui/react'
+import * as React from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
 
-import 'modern-normalize'
+import 'modern-normalize';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Fabric>
+    <FluentProvider theme={teamsLightTheme}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Next.js + Fluent UI Example</title>
       </Head>
       <Component {...pageProps} />
-    </Fabric>
-  )
+    </FluentProvider>
+  );
 }
